52e58c80ddae9776f6b56d0422353861a83e3ee8
huanlv620 <huanlv620@gmail.com>
2025-04-16 14:41:27 +0700

    done ui header co-chua

---
+++ script.js
@@ -0,0 +1,27 @@
+const headerIconMore = document.querySelector(".header__icon-more");
+const headerIconClose = document.querySelector(".header__icon-close");
+const headerInner = document.querySelector(".header__inner");
+
+headerIconMore.onclick = () => {
+  headerIconClose.classList.remove("hide");
+  headerIconMore.classList.add("hide");
+  headerInner.classList.add("xl");
+};
+
+headerIconClose.onclick = () => {
+  headerIconClose.classList.add("hide");
+  headerIconMore.classList.remove("hide");
+  headerInner.classList.remove("xl");
+};
+
+function checkScreenSize() {
+  if (window.innerWidth > 1280) {
+    headerInner.classList.remove("xl");
+  }
+}
+
+// Kiểm tra khi load trang
+checkScreenSize();
+
+// Kiểm tra lại khi resize màn hình
+window.addEventListener("resize", checkScreenSize);
